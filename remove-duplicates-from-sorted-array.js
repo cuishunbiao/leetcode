b10ab6ff901679e6e6c1ae1a67ac4bd4d4b221c2
cuishunbiao <cuishunbiao@ziyun120.com>
2020-12-24 10:03:20 +0800

    完成双指针

--- remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array.js
@@ -9,13 +9,13 @@ var removeDuplicates = function (nums) {
      *  */
     let i = 0;
     for (let j = 0; j < nums.length; j++) {
-        console.log(nums);
         if( nums[i] !== nums[j] ){
+            ++i;
             nums[i] = nums[j]
-            i++;
         }
     }
-    console.log(nums)
+    nums.length = ++i;
+    return nums.length;
 };
 
 removeDuplicates([0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4]);
