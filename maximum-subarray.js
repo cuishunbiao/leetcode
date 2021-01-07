11803ee241ff10f2fd418511a05b59936df62344
cuishunbiao <cuishunbiao@ziyun120.com>
2021-01-07 10:01:18 +0800

    加一

--- maximum-subarray.js
+++ maximum-subarray.js
@@ -7,23 +7,35 @@
  * 
  */
 
-var maxSubArray = function (nums) {
-    let pre = 0;
-    let sum = nums[0];
-    nums.forEach(item=>{
-        console.log({
-            'pre':pre,
-            'sum':sum,
-            'item':item
-        })
+// var maxSubArray = function (nums) {
+//     let pre = 0;
+//     let sum = nums[0];
+//     nums.forEach(item => {
+//         console.log(pre + item,item)
+//         pre = Math.max(pre + item, item);
+//         sum = Math.max(sum, pre)
+//     })
+//     return sum;
+// };
+
+
+//前面的数相加，只要小于后面的数，就前面就没有计算意义了。
+let maxSubArray = function(arr){
+    let sums = arr[0];
+    let total = 0;
+    for(let i of arr){
         debugger
-        pre = Math.max(pre+item,item);
-        sum = Math.max(sum,pre)
-    })
-    return sum;
-};
+        total += i;
+        if( sums > i ){
+            total = sums;
+        }else{
+            total = i;
+        }
+    }
+    return total;
+}
 
 
 
 
-console.log(maxSubArray([1,2,-1,5,-1,2,4]));
+console.log(maxSubArray([1, 2, -1, 5, -1, 2, 4]));
