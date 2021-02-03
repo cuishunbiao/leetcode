36925b4d179f747d7ef3aef605ab2e4e124752cc
cuishunbiao <860026526@qq.com>
2021-02-03 23:10:41 +0800

    拉代码

---
+++ src/150/product-of-array-except-self.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+    // 技巧很巧妙
+    const answer = new Array(nums.length).fill(1)
+    let leftProduct = 1
+    for (let i = 0; i < answer.length; i++) {
+        answer[i] = leftProduct
+        leftProduct *= nums[i]
+    }
+    let rightProduct = 1
+    for (let j = answer.length - 1; j >= 0; j--) {
+        answer[j] = rightProduct
+        rightProduct *= nums[j]
+    }
+    return answer;
+}
+const nums = [1, 2, 3, 4]
+console.log(productExceptSelf(nums));
