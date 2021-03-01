c258e4255b8df36286c85ab52e5474b1fd3a40de
cuishunbiao <cuishunbiao@ziyun120.com>
2021-03-01 11:41:19 +0800

    three num

---
+++ src/150/kth-largest-element-in-an-array.ts
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums: number[], k: number): number {
+    // 你的实现逻辑
+
+    return 0
+}
+const nums = [3, 2, 1, 5, 6, 4],
+    k = 2
+console.log(findKthLargest(nums, k))
