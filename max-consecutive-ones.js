d3624f37e964e799d28874b2612076e833637202
cuishunbiao <cuishunbiao@ziyun120.com>
2021-03-15 09:58:58 +0800

    max-consecutive-ones

---
+++ max-consecutive-ones.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function (nums) {
+    let currentNum = 0;
+    let max = 0;
+    for (let num of nums) {
+        debugger
+        if (num === 1) {
+            currentNum++;
+        } else {
+            max = Math.max(max, currentNum);
+            currentNum = 0;
+        }
+    }
+    return Math.max(max, currentNum);
+};
+console.log(findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1]))
