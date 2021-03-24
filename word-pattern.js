8187f332f8f9d621fb2f5e839fea41c66c581991
cuishunbiao <860026526@qq.com>
2021-03-24 08:10:48 +0800

    word-pattern

--- word-pattern.js
+++ word-pattern.js
@@ -8,37 +8,50 @@
  * 
  */
 var wordPattern = function (pattern, s) {
-    let patternNum = '', sNum = ''
-    for (let code of pattern) {
-        if (code === 'a') {
-            patternNum += '1'
-        } else {
-            patternNum += '0'
+    let stackone = new Map();
+    let stackTwo = new Map();
+    let sArr = s.split(' ')
+    if( pattern.length !== sArr.length ) return false;
+    for (let [i, code] of sArr.entries()){
+        const ch = pattern[i];
+        if (stackone.has(code) && stackone.get(code) !== ch || stackTwo.has(ch) && stackTwo.get(ch) !== code ){
+            return false;
         }
+        stackone.set(code,ch)
+        stackTwo.set(ch,code)
     }
+    return true;
+    // let patternNum = '', sNum = ''
+    // for (let code of pattern) {
+    //     if (code === 'a') {
+    //         patternNum += '1'
+    //     } else {
+    //         patternNum += '0'
+    //     }
+    // }
 
 
-    let codeString = '', sArray = []
-    for (let code of s) {
-        if (code === ' ') {
-            sArray.push(codeString);
-            codeString = ''
-            continue;
-        }
-        codeString += code;
-    }
-    sArray.push(codeString);
+    // let codeString = '', sArray = []
+    // for (let code of s) {
+    //     if (code === ' ') {
+    //         sArray.push(codeString);
+    //         codeString = ''
+    //         continue;
+    //     }
+    //     codeString += code;
+    // }
+    // sArray.push(codeString);
 
-    for (let item of sArray) {
-        if (item === sArray[0]) {
-            sNum += '1'
-        } else {
-            sNum += '0'
-        }
-    }
+    // for (let item of sArray) {
+    //     if (item === sArray[0]) {
+    //         sNum += '1'
+    //     } else {
+    //         sNum += '0'
+    //     }
+    // }
 
-    if (patternNum === sNum) return true;
-    return false;
+    // if (patternNum === sNum) return true;
+    // return false;
 
 };
 
