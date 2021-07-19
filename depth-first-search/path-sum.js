3de81f96c723dc493b8a51542d11006c30aa6d26
cuishunbiao <860026526@qq.com>
2021-07-19 23:03:45 +0800

    更新

---
+++ depth-first-search/path-sum.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ * 
+ * 
+ * 给定一个二叉树，找出其最大深度。
+
+    二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+    说明: 叶子节点是指没有子节点的节点。
+
+    示例：
+    给定二叉树 [3,9,20,null,null,15,7]，
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+    if (root == null) return false
+    // 如果存在 left 
+    if (root.left == null && root.right == null) {
+        console.log(targetSum, root.val)
+        return targetSum === root.val
+    } else {
+        hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.right);
+    }
+};
