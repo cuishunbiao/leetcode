dd00ea118a3af89e4780bc8ac008cff73ab4da89
cuishunbiao <xiang3399nian@gmail.com>
2024-06-14 12:37:12 +0800

    greatest-common-divisor-of-strings

--- src/75/greatest-common-divisor-of-strings.js
+++ src/75/greatest-common-divisor-of-strings.js
@@ -1,16 +1,28 @@
 /**
- * @param {string} str1
- * @param {string} str2
- * @return {string}
- * 先找到短的，循环短的；
- * 两个指针，i 是短的，j 是长的。
- * 移动一个 i，移动多个 j
- * 如果匹配上了，则同时移动 i,j ，并把数据保存到新字符串里，当不匹配的时候，或者到最后，把字符串移动到数组里。
  *
  */
+// 折腾半天，没理解题目要做啥，这个欧几里得的算法，是寻找最大公约数，就是「它们有共同的重复模式」
 var gcdOfStrings = function (str1, str2) {
-    const shortStr = str1.length > str2.length ? str2 : str1
-    for (let i = 0, j = 0; i < shortStr; i++) {}
+    // 在相同重复模式下，如果掉换顺序不相等，则没有重复串
+    if (str1 + str2 != str2 + str1) return ''
+    let n = str1.length
+    let k = str2.length
+    /**
+     * 一直去取最小重复数
+     * AAA 和 AAA => 3%3 = 0; 返回 AAA
+     * AAAA 和 AAA => 4%3 = 1, 继续递归 => 3%1 = 0, => 继续递归， 返回 A
+     * AAAAA 和 AAA => 5%3 = 2, 继续递归 => 3%2 = 1, => 继续递归,  2%1 => 0， 返回 A
+     */
+    const gcds = function (x, y) {
+        if (!y) {
+            return x
+        }
+        return gcds(y, x % y)
+    }
+    let div = gcds(n, k)
+    return str1.slice(0, div)
 }
-const str1 = 'ABCABC',
-    str2 = 'ABC'
+const str1 = 'AAAAA',
+    str2 = 'AAA'
+
+console.log(gcdOfStrings(str1, str2))
