9a5f62620a4e1f70cfb3319ffe1846d84a64330c
cuishunbiao <xiang3399nian@gmail.com>
2024-06-24 12:46:29 +0800

    find-the-difference-of-two-arrays

--- src/75/find-pivot-index.js
+++ src/75/find-pivot-index.js
@@ -1,33 +1,26 @@
 /**
  * @param {number[]} nums
  * @return {number}
+ * 
+ * 先计算所有元素的和；
+ * 进入循环
+ * 左侧每次都相加；右侧每次都计算；
+ * 如果相等，则返回 i；
+ * 否则 -1；
+ * 
  */
 var pivotIndex = function (nums) {
-    if (nums.length < 2) return -1;
-    const len = nums.length - 1
-    let leftCount = 0
-    let rightCount = nums[len];
-
-    let left = 0;
-    let right = len;
-    let middle = -1
-    while (left < right) {
-        debugger
-        if (leftCount === rightCount) {
-            if (left + 1 === right) {
-                middle = left;
-            }
+    const sum = nums.reduce((a, b) => a + b);
+    let leftSum = 0
+    for (let i = 0; i < nums.length; i++) {
+        const rightSum = sum - leftSum - nums[i]
+        if (leftSum === rightSum) {
+            return i
         }
-        if (leftCount <= rightCount) {
-            leftCount += nums[left]
-            left++
-        } else {
-            right--
-            rightCount += nums[right]
-        }
+        leftSum += nums[i]
     }
-    return middle
+    return -1
 };
 
-const nums = [-1, -1, -1, -1, -1, 0]
+const nums = [1, 7, 3, 6, 5, 6]
 console.log(pivotIndex(nums))
