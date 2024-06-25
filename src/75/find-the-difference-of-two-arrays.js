73dec1b9cf493c55c14fb9107f88d7e8a92cfab5
cuishunbiao <xiang3399nian@gmail.com>
2024-06-25 12:42:39 +0800

    find-the-difference-of-two-arrays

--- src/75/find-the-difference-of-two-arrays.js
+++ src/75/find-the-difference-of-two-arrays.js
@@ -2,7 +2,18 @@
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number[][]}
+ * 
+ * 循环两个 map
+ * 
  */
 var findDifference = function (nums1, nums2) {
+    const set1 = new Set(nums1)
+    const set2 = new Set(nums2)
 
+    const set1Result = [...set1].filter(i => !set2.has(i))
+    const set2Result = [...set2].filter(i => !set1.has(i))
+
+    return [set1Result, set2Result]
 };
+const nums1 = [1, 2, 3, 3], nums2 = [1, 1, 2, 2]
+console.log(findDifference(nums1, nums2));
