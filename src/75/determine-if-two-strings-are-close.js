be3c96dbe2e3f797e64c06ba8eb0727c7f606dcc
cuishunbiao <xiang3399nian@gmail.com>
2024-06-26 12:44:35 +0800

    unique-number-of-occurrences

---
+++ src/75/determine-if-two-strings-are-close.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {boolean}
+ */
+var closeStrings = function (word1, word2) {
+
+};
+const word1 = "abc", word2 = "bca"
+console.log(closeStrings(word1, word2))
--- src/75/unique-number-of-occurrences.js
+++ src/75/unique-number-of-occurrences.js
@@ -4,7 +4,20 @@
  */
 var uniqueOccurrences = function (arr) {
     const result = new Map()
-
+    for (let item of arr) {
+        let avalue = result.get(item);
+        if (avalue) {
+            result.set(item, avalue + 1);
+        } else {
+            result.set(item, 1)
+        }
+    }
+    const set = new Set()
+    for (let res of result.values()) {
+        if (set.has(res)) return false;
+        set.add(res)
+    }
+    return true
 };
 const arr = [1, 2, 2, 1, 1, 3]
 console.log(uniqueOccurrences(arr));
