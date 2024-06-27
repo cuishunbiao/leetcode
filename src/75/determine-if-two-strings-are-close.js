071f08f29deeb6994ed883975617ee29d835a814
cuishunbiao <xiang3399nian@gmail.com>
2024-06-27 13:43:33 +0800

    determine-if-two-strings-are-close

--- src/75/determine-if-two-strings-are-close.js
+++ src/75/determine-if-two-strings-are-close.js
@@ -5,31 +5,38 @@
  */
 var closeStrings = function (word1, word2) {
     if (word1.length !== word2.length) return false;
-    const wordMap1 = new Map()
-    for (let item1 of word1) {
-        const isElement = wordMap1.has(item1)
-        if (isElement) {
-            let value = wordMap1.get(item1)
-            wordMap1.set(item1, value + 1)
-        } else {
-            wordMap1.set(item1, 1)
+
+    // 检查字符集
+    const set1 = new Set(word1)
+    const set2 = new Set(word2)
+    if (set1.size !== set2.size) return false;
+    for (let char of set1) {
+        if (!set2.has(char)) return false
+    }
+
+    // 检查字符频率
+    const getFrequencyMap = (word) => {
+        const wordMap = new Map()
+        for (let char of word) {
+            wordMap.set(char, (wordMap.get(char) || 0) + 1)
         }
+        return wordMap
     }
-    const wordMap2 = new Map()
-    for (let item2 of word2) {
-        const isElement = wordMap2.has(item2)
-        if (isElement) {
-            let value = wordMap2.get(item2)
-            wordMap2.set(item2, value + 1)
-        } else {
-            wordMap2.set(item2, 1)
-        }
+    const wordMap1 = getFrequencyMap(word1)
+    const wordMap2 = getFrequencyMap(word2)
+
+    const value1 = Array.from(wordMap1.values()).sort((a, b) => a - b)
+    const value2 = Array.from(wordMap2.values()).sort((a, b) => a - b)
+
+    if (value1.length !== value2.length) {
+        return false;
     }
-    if (wordMap1.size !== wordMap2.size) return false;
-    for (let key of wordMap1.keys()) {
-        if (wordMap1.get(key) !== wordMap2.get(key)) return false;
+
+    for (let i = 0; i < value1.length; i++) {
+        if (value1[i] !== value2[i]) return false;
     }
+
     return true
 };
-const word1 = "cabbba", word2 = "abbccc"
+const word1 = "abc", word2 = "bca"
 console.log(closeStrings(word1, word2))
