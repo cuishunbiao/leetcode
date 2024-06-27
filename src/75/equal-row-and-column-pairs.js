297dcdaddc9d5ce1928748e97866a403bc2f47fd
cuishunbiao <860026526@qq.com>
2024-06-27 22:37:02 +0800

    equal-row-and-column-pairs

---
+++ src/75/equal-row-and-column-pairs.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var equalPairs = function (grid) {
+    for (let item of grid) {
+        console.log(item)
+    }
+};
+const grid = [[3, 2, 1], [1, 7, 6], [2, 7, 7]]
+console.log(equalPairs(grid));
