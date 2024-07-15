f32e4beb39be833cd5b502e0163b3236c06ff6b2
cuishunbiao <xiang3399nian@gmail.com>
2024-07-15 12:47:07 +0800

    feat: pascals-triangle

--- src/75/pascals-triangle.js
+++ src/75/pascals-triangle.js
@@ -3,32 +3,16 @@
  * @return {number[][]}
  */
 var generate = function (numRows) {
-    if (numRows === 0) return []
-    if (numRows === 1) return [[1]]
+    const triangle = [];
 
-    const result = [[1]]
-
-    // if (numRows === 2) return [[1], [1, 1]]
-
-    for (let i = 1; i < numRows; i++) {
-        let rowArr = [[1]]
-        for (let j = 1; j <= i; j++) {
-            const iVal = result[i - 1]
-            const jVal = result[j - 1]
-            debugger
-            console.log(iVal, jVal, Number(iVal) + Number(jVal));
-            rowArr.push(Number(iVal) + Number(jVal))
+    for (let i = 0; i < numRows; i++) {
+        const row = new Array(i + 1).fill(1)
+        for (let j = 1; j < row.length - 1; j++) {
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         }
-        result.push(rowArr);
+        triangle.push(row)
     }
-
-
-    // [0, 0]
-    // [1, 0][1, 1]
-    // [2, 0][2, 1][2, 2]
-    // [3, 0][3, 1][3, 2][3, 3]
-
-
+    return triangle
 };
 
 const numRows = 5
