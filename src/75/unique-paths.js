a25e572fb4d994a189151ec068729f9a36e65af8
cuishunbiao <xiang3399nian@gmail.com>
2024-07-15 21:28:35 +0800

    add code

---
+++ src/75/unique-paths.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ * 
+ * 2, 2 = 2
+ * 3, 2 = 3
+ * 3, 3 = 6
+ * 3 , 7 = 28
+ * 
+ */
+var uniquePaths = function (m, n) {
+
+};
+const m = 3, n = 7
+console.log(uniquePaths(m, n))
