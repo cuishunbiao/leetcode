07b6d6da5847307eb077954d4147d7fbf5f53d93
cuishunbiao <xiang3399nian@gmail.com>
2024-08-06 14:51:29 +0800

    feat: maximum-subarray

--- src/150/maximum-subarray.js
+++ src/150/maximum-subarray.js
@@ -3,7 +3,14 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-
+    if (nums.length === 0) return nums[0]
+    let current = nums[0]
+    let maxNums = nums[0]
+    for (let i = 1; i < nums.length; i++) {
+        current = Math.max(nums[i], current + nums[i])
+        maxNums = Math.max(maxNums, current)
+    }
+    return maxNums
 };
-const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+const nums = [5, 4, -1, 7, 8]
 console.log(maxSubArray(nums));
