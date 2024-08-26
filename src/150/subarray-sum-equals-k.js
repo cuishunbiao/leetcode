1be47277f0fe51c695fbb7709c479f18f7732d57
cuishunbiao <xiang3399nian@gmail.com>
2024-08-26 12:53:37 +0800

    prefix sum

--- src/150/subarray-sum-equals-k.js
+++ src/150/subarray-sum-equals-k.js
@@ -4,28 +4,22 @@
  * @return {number}
  */
 var subarraySum = function (nums, k) {
-    if (!nums.length) return
-    let cache = k
-    let result = 0
-    for (let j = 0; j < nums.length; j++) {
-
-
-        if (nums[j] === k) {
-            result++;
-            continue;
+    let count = 0;
+    let sum = 0;
+    let map = new Map();
+    debugger
+    // Initialize the map with sum 0 occurring once
+    map.set(0, 1)
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+        // if (sum - k) is in the map, them there is a subarray ending at i that sum to k
+        if (map.has(sum - k)) {
+            count += map.get(sum - k)
         }
-        cache -= nums[j];
-        if (cache === 0) {
-            j--;
-            result++;
-            cache = k
-            continue
-        }
-        if (cache > k) {
-            cache = k
-        }
+        // Update the map with the current sum
+        map.set(sum, (map.get(sum) || 0) + 1);
     }
-    return result
+    return count;
 };
-const nums = [-1, -1, 1], k = 0
+const nums = [1, -1, 0], k = 0
 console.log(subarraySum(nums, k))
