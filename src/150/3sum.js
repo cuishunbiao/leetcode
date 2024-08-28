891b46b445cb7a06c19814b9d72a958d44e1c027
cuishunbiao <860026526@qq.com>
2024-08-28 23:29:12 +0800

    add 3sum

--- src/150/3sum.js
+++ src/150/3sum.js
@@ -9,6 +9,13 @@
  * 4. 当三个值相加小于 0 时，说明 left 数小，需要继续加大，则 left+1;
  * 5. 当三个值相加大于 0 时，说明 right 数大，需要继续减小，则 right-1;
  * 6. 当 left 遇到 right 的时候，退出 while 循环，继续执行 for 循环。
+ * 
+ * 第一次存入的时候，存的是数组，但数组有「refaces」的概念，每次存入的数据都是不同的引用，所以每次都会存入值；
+ * 所以需要先转成字符串，通过 Set 来处理成唯一字符串，这个时候还要注意「-1, 0, 1」和「0, -1, 1」是相同的数据，所以需要排序，
+ * 排序后都是「-1, 0, 1」这样的话，同样的数据不会存在两份。
+ * 
+ * 而在最后 return 值的时候，还需要重新把 Set 里所有的内容重新变成 Array 输出。
+ * 
  */
 var threeSum = function (nums) {
     const sortNums = nums.sort((a, b) => a - b)
@@ -18,19 +25,18 @@ var threeSum = function (nums) {
         while (left < right) {
             const val = nums[i] + nums[left] + nums[right]
             if (val === 0) {
-                if (result.has([nums[i], nums[left], nums[right]])) {
-                    result.add([nums[i], nums[left], nums[right]])
-                }
+                const res = JSON.stringify([nums[i], nums[left], nums[right]])
+                result.add(res)
                 left++;
                 right--;
             } else if (val < 0) {
                 left++;
-            } else if (val > 0) {
+            } else {
                 right--;
             }
         }
     }
-    return result
+    return [...result].map(item => JSON.parse(item))
 };
 const nums = [-1, 0, 1, 2, -1, -4]
 console.log(threeSum(nums))
