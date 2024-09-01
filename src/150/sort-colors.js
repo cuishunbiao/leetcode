bc90ecdaafe17e2631d4282d6ec56cd5e96a3a27
cuishunbiao <860026526@qq.com>
2024-09-01 23:03:40 +0800

    add

---
+++ src/150/sort-colors.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+
+};
+const nums = [2, 0, 2, 1, 1, 0]
+console.log(sortColors(nums));
