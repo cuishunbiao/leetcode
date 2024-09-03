10ec1d871cb1e26dee5bd4c7aa5bbc829ce91f19
cuishunbiao <860026526@qq.com>
2024-09-03 22:43:47 +0800

    add

--- src/150/jump-game.js
+++ src/150/jump-game.js
@@ -3,7 +3,14 @@
  * @return {boolean}
  */
 var canJump = function (nums) {
-
+    let maxReach = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (i > maxReach) {
+            return false;
+        }
+        maxReach = Math.max(maxReach, i + nums[i])
+    }
+    return maxReach >= nums.length - 1;
 };
 const nums = [2, 3, 1, 1, 4]
 console.log(canJump(nums))
