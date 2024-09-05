95bfb48f3ad6876141adaca35f74944e31083878
cuishunbiao <xiang3399nian@gmail.com>
2024-09-05 12:45:55 +0800

    add

---
+++ src/150/find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ * 
+ * 定义一个初始化数组为 [-1, -1]
+ * const result = []
+ * 
+ * 如果 nums[i]
+ * 
+ * 
+ */
+var searchRange = function (nums, target) {
+
+};
+const nums = [], target = 0
+console.log(searchRange(nums, target));
