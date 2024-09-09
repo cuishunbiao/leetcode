bc1aaa7ec6ee7c7b47b225c3665cc0ed4caa1813
cuishunbiao <860026526@qq.com>
2024-09-09 22:57:56 +0800

    add code

--- src/150/find-first-and-last-position-of-element-in-sorted-array.js
+++ src/150/find-first-and-last-position-of-element-in-sorted-array.js
@@ -3,15 +3,43 @@
  * @param {number} target
  * @return {number[]}
  * 
- * 定义一个初始化数组为 [-1, -1]
- * const result = []
- * 
- * 如果 nums[i]
- * 
- * 
  */
 var searchRange = function (nums, target) {
-
+    // Find first result 
+    const findFirst = (nums, target) => {
+        let left = 0, right = nums.length - 1, first = -1
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2)
+            if (nums[mid] === target) {
+                first = mid; // if find result, continue find last left number;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return first;
+    }
+    // Find last position number;
+    const findLast = (nums, target) => {
+        let left = 0; right = nums.length - 1, last = -1;
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2)
+            if (nums[mid] === target) {
+                last = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return last
+    }
+    const first = findFirst(nums, target)
+    const last = findLast(nums, target)
+    return [first, last]
 };
-const nums = [], target = 0
+const nums = [5, 7, 7, 8, 8, 10], target = 8
 console.log(searchRange(nums, target));
---
+++ src/150/spiral-matrix.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ * 
+ * 需要设计边界，到了边界减少或者增加数量
+ * 
+ * 定义两个变量，一个横向数量，一个纵向数量；
+ * 循环（循环多少次？）
+ * 当 row 等于
+ * 
+ * column
+ * 
+ * 四个方法：判断当前 row 和 column 的长度，进入不同方法；
+ * row++ 和 row--
+ * column++ 和 column--
+ * 
+ * 
+ */
+var spiralOrder = function (matrix) {
+
+};
