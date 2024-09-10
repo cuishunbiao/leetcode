3fb49e2bc35ae4ca790552c193516fc25d87236a
cuishunbiao <860026526@qq.com>
2024-09-10 22:50:53 +0800

    add code

---
+++ src/150/continuous-subarray-sum.ts
@@ -0,0 +1,4 @@
+function checkSubarraySum(nums: number[], k: number): boolean {
+  
+    return true
+};
--- src/150/kth-largest-element-in-an-array.ts
+++ src/150/kth-largest-element-in-an-array.ts
@@ -4,10 +4,9 @@
  * @return {number}
  */
 var findKthLargest = function (nums: number[], k: number): number {
-    // 你的实现逻辑
-
-    return 0
+    nums.sort((a, b) => b - a);
+    return nums[k-1]
 }
-const nums = [3, 2, 1, 5, 6, 4],
-    k = 2
-console.log(findKthLargest(nums, k))
+const defaultNums = [3,2,3,1,2,4,5,5,6,7,7,8,2,3,1,1,1,10,11,5,6,2,4,7,8,5,6]
+, k = 2
+console.log(findKthLargest(defaultNums, k))
